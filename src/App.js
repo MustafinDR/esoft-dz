06277add0c9440d37b924d8bfb121e226bceeded
Denislam Mustafin <mustafindr1995@gmail.com>
2024-04-25 15:04:50 +0500

    add cdalal

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {Competenc} from './components/competenc.jsx';
 import { listStudiet, listStudy } from './components/data.jsx';
 import {BigCard} from './components/BigCard.jsx'
+import cdelal from './components/cdelal.jpg'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BigCard></BigCard>
       <Competenc list = {listStudiet} name = {'Изучено'}></Competenc>
       <Competenc list = {listStudy} name = {'Изучить'}></Competenc>
+      <img src={cdelal} className="cdelal"/>
     </div>
   );
 }
